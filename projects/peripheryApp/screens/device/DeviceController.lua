b1d4455b9a61d508c5a3023de4efd54a9925caa5
Joseph Stewart <josephlikescode@gmail.com>
2025-02-11 17:15:16 -0800

    Don't delete device controllers once constructed.

--- projects/peripheryApp/screens/device/DeviceController.lua
+++ projects/peripheryApp/screens/device/DeviceController.lua
@@ -12,8 +12,11 @@ function DeviceController:init(owningFrame, appController)
 		app = appController,
 		id = nil,
 		deviceController = nil,
+		deviceFrame = nil,
 		device = nil,
 		deviceType = nil,
+		cachedDeviceFrames = {},
+		cachedDeviceControllers = {}
 	})
 end
 
@@ -21,8 +24,20 @@ function DeviceController:onShow(options)
 	self.id = options.id
 	self.view.label:setText(options.label or ("Device " .. options.id))
 
-	if self.deviceController ~= nil then
-		self.view.content:removeChildren()
+	if self.deviceFrame then
+		self.deviceFrame:hide();
+	end
+
+	if self.deviceController and self.deviceController.onHide then
+		self.deviceController:onHide()
+	end
+
+	if self.cachedDeviceFrames[options.id] then
+		if self.cachedDeviceControllers[options.id] and self.cachedDeviceControllers[options.id].onShow then
+			self.cachedDeviceControllers[options.id]:onShow()
+		end
+		self.cachedDeviceFrames[options.id]:show()
+		return
 	end
 
 	self.device = self.app.periphery:get(options.id)
@@ -40,8 +55,21 @@ function DeviceController:onShow(options)
 		return
 	end
 
+	self.deviceFrame = self.view.content
+		:addFrame()
+		:setPosition(1, 1)
+		:setSize("parent.w", "parent.h")
+		:setBackground(colors.white)
+
 	self.deviceController = UIController
-		:new(self.view.content, self, self.device)
+		:new(self.deviceFrame, self, self.device)
+
+	if self.deviceController.onShow then
+		self.deviceController:onShow()
+	end
+
+	self.cachedDeviceControllers[options.id] = self.deviceController
+	self.cachedDeviceFrames[options.id] = self.deviceFrame
 end
 
 function DeviceController:back()
